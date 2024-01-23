c69c77611d827eebb04f8120edb0000b1a12016f
ChillingTime <102903069+NotForGetNow@users.noreply.github.com>
2024-01-23 21:18:05 +0600

    Add files via upload

---
+++ JS/js/js.js
@@ -0,0 +1,159 @@
+// 1 задание
+
+// let num1 = +prompt("введите число 1")
+// let result = num * num
+// alert(result)
+
+
+// 2 задание
+
+// let num1 = +prompt("введите число 1")
+// let num2 = +prompt("введите число 2")
+
+// let result = (num1 + num2) / 2
+// alert(result)
+
+
+// 3 задание
+
+// let num = +prompt("введите длину квадрата")
+// let result = num * num
+
+// alert(result)
+
+
+// 4 задание
+
+// let num = +prompt("Введите растояние (км) ")
+// const num1 = 0.621371
+// let result = num * num1
+
+// alert(result)
+
+
+// 5 задание
+
+// let num1 = +prompt("Введите число 1")
+// let num2 = +prompt("Введите число 2")
+// let result1 = num1 + num2
+// let result2 = num1 - num2
+// let result3 = num1 * num2
+// let result4 = num1 / num2
+
+// alert(result1), alert(result2), alert(result3), alert(result4)
+
+
+// 6 задание
+
+// let a = +prompt("Введите число a")
+// let b = +prompt("Введите число b")
+
+// let result = (-b) / a
+// alert(result)
+
+
+// 7 задание
+
+// let hour = +prompt("Введите ваше время - часы")
+// let minute = +prompt("Введите ваше время - минуты")
+
+// const timeHour = 24
+// const timeMinute = 60
+
+// let resultHour = hour - timeHour
+// let resultMinute = minute - timeMinute
+// let result = (-resultHour-1) + " " + "часа" + " " + (-resultMinute) + " " + "минут"
+// alert(result)
+
+
+// 8 задание
+
+// let num = +prompt("Введите трехзначное число")
+
+// let result = num / 10 % 10
+// result = parseInt(result)
+// alert(result)
+
+// 9 задание
+
+// let num = +prompt("Введите пятизначное число")
+
+// let firstNum = num % 10
+// let lastNum = num / 10
+
+// let result = firstNum + "" + parseInt(lastNum)
+
+// alert(result)
+
+// 10 задание
+
+let num = +prompt("Введите зарплату работника")
+
+let sum = num + 10%
+let res = sum * 30
+
+alert(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let nameUser = "kanat"
+
+// let nameUser1 = "Anuar"
+
+// alert(nameUser1, nameUser)
+
+// confirm("Ты человек?")
+
+// let name = prompt("напиши имя")
+
+// let name = +prompt("Сколько тебе лет")
+
+// alert(name + 5)
+
+// let nick = "k1mone"
+// console.log(typeof(nick))
+
+// let age = 200
+// console.log(typeof(age))
+
+// let name = nick
+// console.log(typeof(nick))
+
+
+// let num1 = 1
+// let num2 = "1"
+
+// let sum = num1 + Number(num2)
+
+// console.log(sum)
+
+// let firstName = "Kanat"
+// let lastName = "Anuar"
+
+// alert(firstName + " " + lastName)
